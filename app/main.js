3a0dbeda88c9fc7036b094b6785204a6ca09d46f
obalbous <oba@squid>
2014-08-04 18:19:08 +0200

    Added datatable view and analysis

--- app/main.js
+++ app/main.js
@@ -1,18 +1,60 @@
 $( document ).ready(function() {
     
-    var loginView, statusView, contentView, config, filtersView, datePicker, periodView, selectionView;
+    var analysis, totalAnalysis, filters, loginView, statusView, contentView, config, filtersView, datePicker, periodView, selectionView, dataTableView;
     
     config = {
         "customerId" : null,
         "clientId" : "local",
         "projectId" : "els_flow_1",
     };
-
+    
     var domainId = "usage";
+    
+    var defaultSelection = {
+            "facets" : [ {
+                "dimension" : {
+                    "id" : {
+                        "projectId" : config.projectId,
+                        "domainId" : domainId,
+                        "dimensionId" : "session_cre_dt"
+                    }
+                },
+                "selectedItems" : [ {
+                    "type" : "i",
+                    "lowerBound" : "2014-04-15T00:00:00.000Z",
+                    "upperBound" : "2014-04-30T00:00:00.000Z"
+                } ]
+            }, {
+                "dimension" : {
+                    "id" : {
+                        "projectId" : config.projectId,
+                        "domainId" : domainId,
+                        "dimensionId" : "usergroup"
+                    }
+                },
+                "selectedItems" : [ {
+                    "type" : "v",
+                    "id" : -1,
+                    "value" : "Customer"
+                } ]
+            } ]
+    };
+    
+    analysis = new squid_api.controller.analysisjob.AnalysisModel();
+    analysis.setProjectId(config.projectId);
+    analysis.setDomainIds([domainId]);
+    analysis.setDimensionIds(["step0","step1","step2"]);
+    analysis.setMetricIds(["count"]);
+    
+    totalAnalysis = new squid_api.controller.analysisjob.AnalysisModel();
+    totalAnalysis.setProjectId(config.projectId);
+    totalAnalysis.setDomainIds([domainId]);
+    totalAnalysis.setMetricIds(["count"]);
 
-    var filters = new squid_api.controller.facetjob.FiltersModel();
+    filters = new squid_api.controller.facetjob.FiltersModel();
     filters.setProjectId(config.projectId);
     filters.setDomainIds([domainId]);
+    filters.set("selection" , defaultSelection);
     
     /*
      * Declare the views 
@@ -54,6 +96,16 @@ $( document ).ready(function() {
         model : filters
     });
     
+    dataTableView = new squid_api.view.DataTableView({
+        el : $('#analysis'),
+        model : analysis
+    });
+    
+    totalView = new squid_api.view.DataTableView({
+        el : $('#total'),
+        model : totalAnalysis
+    });
+    
     /*
      * Controller part
      */
@@ -62,7 +114,11 @@ $( document ).ready(function() {
         // performed when login is updated
         if (model.get("login")) {
             // login ok
+            // compute the filters
             squid_api.controller.facetjob.compute(filters);
+            // compute the analysis
+            squid_api.controller.analysisjob.computeAnalysis(analysis, filters);
+            squid_api.controller.analysisjob.computeAnalysis(totalAnalysis, filters);
         }
     });
     
